5336aa0ed47b293010f89f734fced1f9dfad58a8
Dorgazi <Dorgazi@seas.upenn.edu>
2025-05-26 19:04:19 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,6 @@ const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname));
 
-app.listen(port, () => {
-  console.log(`E-commerce site running on http://localhost:${port}`);
+app.listen(8080, '0.0.0.0', () => {
+  console.log(`E-commerce site running on http://0.0.0.0:8080`);
 });
